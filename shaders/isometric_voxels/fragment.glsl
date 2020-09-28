07449587282b02d1f77a337c7215594b9d222b1e
Merlin Brandt <merlin.brandt@hotmail.de>
2020-09-28 03:47:32 +0200

    Update fragment.glsl

--- shaders/isometric_voxels/fragment.glsl
+++ shaders/isometric_voxels/fragment.glsl
@@ -14,6 +14,7 @@ void main() {
     float time = time * 10.f;
     int itime = int(time);
     fragColor = vec4(vColor, 1.0);
+    fragColor.b = 1;
 }
 
 // https://stackoverflow.com/questions/15095909/from-rgb-to-hsv-in-opengl-glsl
